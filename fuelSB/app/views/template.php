934e65f9046b1f0a405fecd85e1a2154239d407a
jonquill howlett <jonquill.howlett@gmail.com>
2022-03-24 18:47:43 -0600

    links

--- fuelSB/app/views/template.php
+++ fuelSB/app/views/template.php
@@ -12,9 +12,9 @@
     <nav>
         <ul>
             <!-- Will need to change the links for SpudBuds -->
-            <li><a href="https://cs.colostate.edu:4444/~jonquill/m1/fuelviews/index.php/spudbuds/index.php">SpudBuds</a></li>
-            <li><a href="https://cs.colostate.edu:4444/~jonquill/m1/fuelviews/index.php/spudbuds/bios.php">About Us</a></li>
-            <li><a href="https://cs.colostate.edu:4444/~jonquill/m1/fuelviews/index.php/spudbuds/mosaic.php">Mosaic</a></li>
+            <li><a href="SpudBuds/fuelSB/app/views/spudbuds/index.php">SpudBuds</a></li>
+            <li><a href="SpudBuds/fuelSB/app/views/spudbuds/bios.php">About Us</a></li>
+            <li><a href="SpudBuds/fuelSB/app/views/spudbuds/mosaic.php">Mosaic</a></li>
             
         </ul>
     </nav>
