337ddd62c77909683411d58fcdd5e42eb8e30ec9
Samuel White <sammytwhite9@gmail.com>
2022-03-29 20:40:03 -0600

    First Table with drop down menus

--- fuelSB/app/views/spudbuds/mosaic.php
+++ fuelSB/app/views/spudbuds/mosaic.php
@@ -15,35 +15,41 @@
 </form>
 
 <?php
+    $color;
+    //Wokrong on the first table
+    if(isset($_GET['color'])){
+        $color = $_GET['color'];
+        echo "<br>testing color should be blahhhh given: ", $color;
 
+    }
+    
+    echo "<table style=\"width:100%\"border =\"1px\" >";
+
+    $flag = false;
+    for($x=0; $x <= $color; $x++){
+        echo "<tr>";
+        echo "<td style=\"width:20%\">; 
+        <select name=\"flagcolors\">
+            <option value=\"red\">Red</option>
+            <option value=\"orange\">Orange</option>
+            <option value=\"yellow\">Yellow</option>
+            <option value=\"green\">Green</option>
+            <option value=\"blue\">Blue</option>
+            <option value=\"purple\">Purple</option>
+            <option value=\"grey\">Grey</option>
+            <option value=\"teal\">Teal</option>
+        </select>
+        </td>";
+        echo "<td style=\"width:80%\"> Bitch </td>";
+        echo"</tr>";
+    }
+    echo"</table>";
 
     $colors = array("red", "orange", "yellow", "green", "blue", "purple", "grey", "brown", "black", "teal");
-
-
-
-
-
-
     if(isset($_GET['number'])){
         $number = $_GET['number'];
         if($number >= 1 && $number <=26){
             echo "testing number should be whatever I give and submit: " , $number, "<br>";
-            
-            echo "<table border =\"1px\" >";
-            for($x=0; $x <= 5; $x++){
-                echo "<tr>";
-        
-                for($y=0; $y< 2; $y++){
-                    echo "<td> Fuck </td>";
-                }
-                echo"</tr>";
-            }
-            
-            echo"</table>";
-        
-
-
-            
         }
         else{
             echo "you fucked up";
@@ -51,16 +57,5 @@
 
         
     }
-
-
-    
-
-    if(isset($_GET['color'])){
-        $color = $_GET['color'];
-        echo "<br>testing color should be blahhhh given: ", $color;
-
-    }
-    
-    
 
 ?>
