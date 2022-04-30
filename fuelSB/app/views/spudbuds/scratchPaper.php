e58a29758608907fac68c7f8e97de224e826b171
jonquill howlett <jonquill.howlett@gmail.com>
2022-04-29 18:45:27 -0600

    about to fuck shit up

---
+++ fuelSB/app/views/spudbuds/scratchPaper.php
@@ -0,0 +1,50 @@
+<!--Currently working with the divs -->
+<main>
+    <div id=jon>
+    <h2> Jonquill Howlett</h2>
+        <p> <?php echo Asset::img("bio_pic.jpg");?> </p>
+        <p> 
+            <ul> 
+                <li>Third Year Student</li>
+                <li>Favorite Color is Purple </li>
+                <li>Favorite TV Show is currently Criminal Minds</li>
+            </ul>
+        </p>
+    </div>
+
+    <div id=sam>
+    <h2> Sam White</h2>
+        <p> <?php echo Asset::img("sammy.jpg");?></p> 
+        <p>
+            <ul>
+                <li>Second Year Computer Science Major</li>
+                <li>Favorite Color is Dark Gray</li>
+                <li>My favorite TV Show is currently Formula One: Drive to survive</li>
+            </ul>
+        </p>
+    </div>
+    <div id=ellery>
+    <h2> Ellery Osborne</h2>
+        <p> <?php echo Asset::img("ellery.jpg");?></p> 
+        <p>
+            <ul>
+                <li>Third Year Student</li>
+                <li>Favorite Color is Slate Blue</li>
+                <li>My favorite TV Show will always be Grey's Anatomy</li>
+            </ul>
+        </p>
+    </div>
+
+    <div id=thad>
+    <h2> Thad Avery</h2>
+    <p><?php echo Asset::img("thad.jpg");?></p>
+        <p>
+            <ul>
+                <li>Junior in Computer Science</li>
+                <li>Recieved participation at the science fair when I was 5</li>
+                <li>My favorite movie is Grown Ups</li>
+                <li>Have mullet</li>
+            </ul>
+        </p>
+    </div>
+</main>
