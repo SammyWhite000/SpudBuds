e52ea8d80fe8b722d256bb0ee203913075426295
jonquill howlett <jonquill.howlett@gmail.com>
2022-04-29 18:38:24 -0600

    two different options for bios

--- fuelSB/app/views/spudbuds/bios_table.php
+++ fuelSB/app/views/spudbuds/bios_table.php
@@ -1,49 +1,66 @@
 <main>
-    <div id=jon>
-    <h2> Jonquill Howlett</h2>
-        <p> <?php echo Asset::img("bio_pic.jpg");?> </p>
-        <p> 
+<table>
+<tbody>
+  <tr>
+    <td><h2> Jonquill Howlett</h2></td>
+    <td></td>
+    <td><h2> Sam White</h2></td>
+  </tr>
+  <tr>
+    <td><p> <?php echo Asset::img("bio_pic.jpg");?> </p></td>
+    <td></td>
+    <td><p> <?php echo Asset::img("sammy.jpg");?></p></td>
+  </tr>
+  <tr>
+    <td><p> 
             <ul> 
                 <li>Third Year Student</li>
                 <li>Favorite Color is Purple </li>
                 <li>Favorite TV Show is currently Criminal Minds</li>
             </ul>
-        </p>
-    </div>
-
-    <div id=sam>
-    <h2> Sam White</h2>
-        <p> <?php echo Asset::img("sammy.jpg");?></p> 
-        <p>
+        </p></td>
+    <td></td>
+    <td><p>
             <ul>
                 <li>Second Year Computer Science Major</li>
                 <li>Favorite Color is Dark Gray</li>
                 <li>My favorite TV Show is currently Formula One: Drive to survive</li>
             </ul>
-        </p>
-    </div>
-    <div id=ellery>
-    <h2> Ellery Osborne</h2>
-        <p> <?php echo Asset::img("ellery.jpg");?></p> 
-        <p>
+        </p></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><h2> Ellery Osborne</h2></td>
+    <td></td>
+    <td><h2> Thad Avery</h2></td>
+  </tr>
+  <tr>
+    <td><p> <?php echo Asset::img("ellery.jpg");?></p> </td>
+    <td></td>
+    <td><p><?php echo Asset::img("thad.jpg");?></p></td>
+  </tr>
+  <tr>
+    <td> <p>
             <ul>
                 <li>Third Year Student</li>
                 <li>Favorite Color is Slate Blue</li>
                 <li>My favorite TV Show will always be Grey's Anatomy</li>
             </ul>
-        </p>
-    </div>
-
-    <div id=thad>
-    <h2> Thad Avery</h2>
-    <p><?php echo Asset::img("thad.jpg");?></p>
-        <p>
+        </p></td>
+    <td></td>
+    <td><p>
             <ul>
                 <li>Junior in Computer Science</li>
                 <li>Recieved participation at the science fair when I was 5</li>
                 <li>My favorite movie is Grown Ups</li>
                 <li>Have mullet</li>
             </ul>
-        </p>
-    </div>
+        </p></td>
+  </tr>
+</tbody>
+</table>
 </main>
